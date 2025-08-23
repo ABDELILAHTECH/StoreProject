9883014cf8b724aec08522e2eab9f97bb4c8ea3e
abdelilah <abdelilahtech1@gmail.com>
2025-08-23 11:16:48 +0100

    Récupération des produits depuis fakestoreapi

--- StoreProject/src/App.jsx
+++ StoreProject/src/App.jsx
@@ -2,12 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import Cart from "./Pages/Cart"
 import Favorites from "./Pages/Favorites"
-import { createContext } from "react"
-export default function App() {
-  const ProductContext = createContext([])
+import { createContext, useEffect, useState } from "react"
 
+const ProductContext = createContext([])
+
+
+export default function App() {
+  let [storeProducts,setStoreProducts] = useState([])
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())            
+            .then(json=>setStoreProducts(json))
+           
+            
+   },[])
+    
+  useEffect(()=>{
+     console.log(storeProducts);
+
+  },[storeProducts]) 
   return ( 
-    <ProductContext.Provider >
+    <ProductContext.Provider value={storeProducts}>
        <BrowserRouter>
           <Routes>
             <Route path="*" />
