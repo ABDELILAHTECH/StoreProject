b8278c82cf2b3fa15c411ed309716b6083f4d05a
abdelilah <abdelilahtech1@gmail.com>
2025-08-23 10:46:47 +0100

    Ajout de la navigation entre Home, Cart et Favorites avec React Router

--- StoreProject/src/App.jsx
+++ StoreProject/src/App.jsx
@@ -1,8 +1,16 @@
-
+import { BrowserRoutes, Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import Cart from "./Pages/Cart"
+import Favorites from "./Pages/Favorites"
 export default function App() {
-  return (
-    <div>
-      
-    </div>
+  return ( 
+    <BrowserRoutes>
+        <Routes>
+          <Route path="*" />
+          <Route path="/" element={<Home/>} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+    </BrowserRoutes>
   )
 }
