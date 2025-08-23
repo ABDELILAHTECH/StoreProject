f8c99f300669d05dad9a282db04465a4beebde6f
abdelilah <abdelilahtech1@gmail.com>
2025-08-23 10:53:50 +0100

    Mise en place du Context API pour partager les produits

--- StoreProject/src/App.jsx
+++ StoreProject/src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRoutes, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import Cart from "./Pages/Cart"
 import Favorites from "./Pages/Favorites"
+import { createContext } from "react"
 export default function App() {
+  const ProductContext = createContext([])
+
   return ( 
-    <BrowserRoutes>
-        <Routes>
-          <Route path="*" />
-          <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-    </BrowserRoutes>
+    <ProductContext.Provider >
+       <BrowserRouter>
+          <Routes>
+            <Route path="*" />
+            <Route path="/" element={<Home/>} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </BrowserRouter>
+    </ProductContext.Provider>
   )
 }
